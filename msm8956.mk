57054c9a1c8fc236b877ad3c8afb4b0e411d3938
Adarsh-MR <adarshmr1998@gmail.com>
2017-07-25 17:34:24 +0000

    msm8956-common: update CAF tag
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -32,7 +32,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # CAF REVISION
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.caf.branch=LA.BR.1.3.6_rb1.13
+    ro.caf.branch=LA.BR.1.3.6_rb1.20
 
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
