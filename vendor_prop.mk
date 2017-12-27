9fdb181ed494a45f9669cddbd58a5ee6b61aa33f
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-28 00:13:23 +0200

    msm8956-common: Use full path for FRP partition
    Change-Id: I6757e814c977112e0d4da9856751d9c1f29d7620

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -82,7 +82,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/config
+    ro.frp.pst=/dev/block/platform/soc.0/7824900.sdhci/by-name/config
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
