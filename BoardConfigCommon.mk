BirdSanctuary/device_xiaomi_msm8956-common

msm8956-common: Mark camera-related processes as API level 23.
Our camera blobs trigger some checks for broken behavior (destroyed
mutex usage, invalid pthread usage) present in newer API levels.

Change-Id: I8705437e0a5c6d80b86748bff559ce1e8a7bf716

BoardConfigCommon.mk
@@ -121,6 +121,10 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# Camera
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+	/system/bin/mm-qcamera-daemon=23
+
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 TARGET_CONTINUOUS_SPLASH_ENABLED := true

khusnan21/device_xiaomi_msm8956-common-1

BoardConfigCommon.mk
@@ -121,6 +121,10 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# Camera
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+	/vendor/bin/mm-qcamera-daemon=23
+
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 TARGET_CONTINUOUS_SPLASH_ENABLED := true