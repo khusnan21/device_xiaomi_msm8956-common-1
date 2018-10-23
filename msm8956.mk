e4ae57d815ae39b8157d931b153a7d0bebc64a7c
Adarsh-MR <adarshmr1998@gmail.com>
2018-10-23 09:51:33 +0000

    msm8956-common: Remove local copy of hidl.base and hidl.manager
    * caf still provides their own in system/libhidl
    
    This reverts commit dca758169bf4be96ea27fcfa1c78d9ba4e5f9513.

--- msm8956.mk
+++ msm8956.mk
@@ -219,9 +219,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
