4d79962d4467f8b305013bc21ab6c9d1838bb1e7
Mikhail Naganov <mnaganov@google.com>
2018-11-24 14:52:45 +0000

    msm8956-common: Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -92,8 +92,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
     audio.primary.msm8952 \
     audio.r_submix.default \
