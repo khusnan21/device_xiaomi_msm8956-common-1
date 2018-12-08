65b43c40766a7db0389839de8ba692638d830299
Adarsh-MR <adarshmr1998@gmail.com>
2018-12-08 16:25:09 +0000

    msm8956-common: disable PRODUCT_ENFORCE_RRO_TARGETS
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -19,7 +19,7 @@
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
+# PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
 # Get non-open-source specific aspects
